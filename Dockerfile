5c0859abe52cdcced1564f8f4764d4a2a8c74f3e
Jairo <jairoandre@gmail.com>
2024-02-18 21:27:43 -0300

    sqlx refactory

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM rust:1.76.0 as builder
-WORKDIR /usr/src/rust_docker
-RUN apt install libpq-dev
+FROM clux/muslrust
+WORKDIR /app
+# silly docker copy about directory
+# sh -c 'cd .. && docker build --network host -t demo -f actixweb-sqlx-jwt/Dockerfile .'
 COPY . . 
-RUN cargo install --path .
+RUN ls -lah && RUSTFLAGS='-C target-feature=+crt-static' cargo build --release && ls -lah target/*
 
-FROM debian:buster-slim
-COPY --from=builder /usr/local/cargo/bin/rust_docker /usr/local/bin/rust_docker
-EXPOSE 8080
-CMD ["rust_docker"]
+FROM alpine:latest  
+RUN apk --no-cache add ca-certificates
+WORKDIR /opt/
+COPY --from=0 /app/target/x86_64-unknown-linux-musl/release/rb2024 /usr/bin/rb2024
+CMD ["rb2024"]
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,28 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
-use diesel_migrations::{EmbeddedMigrations, MigrationHarness};
+use actix_web::{web::Data, App, HttpServer};
 use env_logger::Env;
-use serde::Serialize;
-#[macro_use]
-extern crate diesel_migrations;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod handlers;
-mod models;
-mod repository;
 
-#[derive(Serialize)]
-pub struct Response {
-    status: String,
-    message: String,
-}
-
-type DB = diesel::pg::Pg;
-const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
-
-fn run_migrations(connection: &mut impl MigrationHarness<DB>) {
-    let _ = connection.run_pending_migrations(MIGRATIONS);
-}
-
-#[get("/health")]
-async fn health() -> impl Responder {
-    HttpResponse::Ok().json(Response {
-        status: "ok".to_string(),
-        message: "Server is running".to_string(),
-    })
-}
-
-async fn not_found_error() -> Result<HttpResponse> {
-    Ok(HttpResponse::NotFound().json(Response {
-        status: "error".to_string(),
-        message: "Not Found".to_string(),
-    }))
+pub struct AppState {
+    db: Pool<Postgres>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let rinha_db = repository::db::Database::new();
-    run_migrations(&mut rinha_db.pool.get().unwrap());
-    let app_data = web::Data::new(rinha_db);
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("Trying to connect to the database: {}", &database_url);
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
+
     HttpServer::new(move || {
         App::new()
-            .app_data(app_data.clone())
+            .app_data(Data::new(AppState { db: pool.clone() }))
             .configure(handlers::handlers::init_routes)
-            .service(health)
-            .default_service(web::route().to(not_found_error))
             .wrap(actix_web::middleware::Logger::default())
     })
     .bind(("0.0.0.0", 8080))?
